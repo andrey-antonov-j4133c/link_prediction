42f810d30a2ce6cdfc638d2335311e872c671a0b
andrey-antonov-j4133c <a.antonov@niuitmo.ru>
2021-08-04 08:40:02 +0300

    New graphs

--- lib/link_prediction_models/RandomForestOnFeatures.py
+++ lib/link_prediction_models/RandomForestOnFeatures.py
@@ -3,21 +3,21 @@ from sklearn.ensemble import RandomForestClassifier
 
 class RandomForestOnFeatures:
     def __init__(self, X_train, Y_train, n_estimators=100, criterion='gini', max_depth=None, random_state=0) -> None:
-        self.forest = RandomForestClassifier(
+        self.clf = RandomForestClassifier(
             n_estimators=n_estimators,
             criterion=criterion,
             max_depth=max_depth,
             random_state=random_state)
 
-        self.forest.fit(X_train, Y_train)
+        self.clf.fit(X_train, Y_train)
 
 
     def predict_proba(self, X_test):
-        return self.forest.predict_proba(X_test)[:, 1]
+        return self.clf.predict_proba(X_test)[:, 1]
 
 
     def accuracy_score(self, X_test, Y_test):
-        return self.forest.score(X_test, Y_test)
+        return self.clf.score(X_test, Y_test)
 
     def get_model(self):
-        return self.forest
+        return self.clf
