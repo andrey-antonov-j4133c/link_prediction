d4ab5c9026cf01adf79074da2a1daf0af375793c
andrey-antonov-j4133c <a.antonov@niuitmo.ru>
2021-12-26 12:17:43 +0300

    Attributed networks as a module

---
+++ lib/main.py
@@ -0,0 +1,32 @@
+import os
+import shutil
+
+from SETTINGS import * 
+import logging as log
+
+from Experiment.experiment import Experiment
+from Generator.RealWorldAttributedNetworkGenerator import RealWorldAttributedNetorkGeberator
+
+def __main__():
+    if VEBROSE:
+        log.basicConfig(format="%(levelname)s: %(message)s", level=log.DEBUG)
+        log.info("Verbose output.")
+    else:
+        log.basicConfig(format="%(levelname)s: %(message)s")
+
+    if os.path.exists(RESULT_PATH) and os.path.isdir(RESULT_PATH):
+        shutil.rmtree(RESULT_PATH)
+
+    experiments = [
+        Experiment(RealWorldAttributedNetorkGeberator(), {'path': './data/', 'dataset_name': 'citeseer'}),
+        Experiment(RealWorldAttributedNetorkGeberator(), {'path': './data/', 'dataset_name': 'cora_ml'}),
+        Experiment(RealWorldAttributedNetorkGeberator(), {'path': './data/', 'dataset_name': 'cora'}),
+        Experiment(RealWorldAttributedNetorkGeberator(), {'path': './data/', 'dataset_name': 'dblp'}),
+        Experiment(RealWorldAttributedNetorkGeberator(), {'path': './data/', 'dataset_name': 'pubmed'})
+    ]
+
+    for i, e in enumerate(experiments):
+        os.makedirs(RESULT_PATH + str(i) + '/')
+        e.run(i)
+
+__main__()
