85933f2d8f2a047a9dc8304126b27d3d95b31ef4
andrey-antonov-j4133c <a.antonov@niuitmo.ru>
2022-02-02 10:49:36 +0300

    Additions

--- lib/pre_compute_datasets.py
+++ lib/pre_compute_datasets.py
@@ -2,14 +2,44 @@ import logging as log
 import os
 import shutil
 
+from data_formatting.real_world_attributed_networks import RealWorldAttrFormatter
+from data_formatting.real_world_non_attributed_networks import RealWorldNonAttrFormatter
 from data_formatting.synthetic_networks import SyntheticFormatter
 
 from settings import *
 
 datasets = [
-    (SyntheticFormatter,
-     True,
-     {'path': DATA_PATH + 'synthetic/acMark/', 'dataset_name': 'acMark-a=0.1;b=0.1;s=0.1;o=0.1'})
+    (SyntheticFormatter, True, {'path': DATA_PATH + 'synthetic/acMark/', 'dataset_name': 'acMark-a=0.1;b=0.1;s=0.1;o=0.1'}),
+    #(SyntheticFormatter, True, {'path': DATA_PATH + 'synthetic/acMark/', 'dataset_name': 'acMark-a=0.2;b=0.1;s=0.1;o=0.1'}),
+    #(SyntheticFormatter, True, {'path': DATA_PATH + 'synthetic/acMark/', 'dataset_name': 'acMark-a=0.3;b=0.1;s=0.1;o=0.1'}),
+    #(SyntheticFormatter, True, {'path': DATA_PATH + 'synthetic/acMark/', 'dataset_name': 'acMark-a=0.4;b=0.1;s=0.1;o=0.1'}),
+    #(SyntheticFormatter, True, {'path': DATA_PATH + 'synthetic/acMark/', 'dataset_name': 'acMark-a=0.5;b=0.1;s=0.1;o=0.1'}),
+    #(SyntheticFormatter, True, {'path': DATA_PATH + 'synthetic/acMark/', 'dataset_name': 'acMark-a=0.6;b=0.1;s=0.1;o=0.1'}),
+    #(SyntheticFormatter, True, {'path': DATA_PATH + 'synthetic/acMark/', 'dataset_name': 'acMark-a=0.7;b=0.1;s=0.1;o=0.1'}),
+    #(SyntheticFormatter, True, {'path': DATA_PATH + 'synthetic/acMark/', 'dataset_name': 'acMark-a=0.8;b=0.1;s=0.1;o=0.1'}),
+    #(SyntheticFormatter, True, {'path': DATA_PATH + 'synthetic/acMark/', 'dataset_name': 'acMark-a=0.9;b=0.1;s=0.1;o=0.1'}),
+    #(SyntheticFormatter, True, {'path': DATA_PATH + 'synthetic/acMark/', 'dataset_name': 'acMark-a=1;b=0.1;s=0.1;o=0.1'}),
+
+    (SyntheticFormatter, False, {'path': DATA_PATH + 'synthetic/LFR/', 'dataset_name': 'LFR-t1=3;t2=1.5;mu=0.1;average_degree=5;'}),
+    #(SyntheticFormatter, False, {'path': DATA_PATH + 'synthetic/LFR/', 'dataset_name': 'LFR-t1=3;t2=1.5;mu=0.2;average_degree=5;'}),
+    #(SyntheticFormatter, False, {'path': DATA_PATH + 'synthetic/LFR/', 'dataset_name': 'LFR-t1=3;t2=1.5;mu=0.3;average_degree=5;'}),
+    #(SyntheticFormatter, False, {'path': DATA_PATH + 'synthetic/LFR/', 'dataset_name': 'LFR-t1=3;t2=1.5;mu=0.4;average_degree=5;'}),
+    #(SyntheticFormatter, False, {'path': DATA_PATH + 'synthetic/LFR/', 'dataset_name': 'LFR-t1=3;t2=1.5;mu=0.5;average_degree=5;'}),
+    #(SyntheticFormatter, False, {'path': DATA_PATH + 'synthetic/LFR/', 'dataset_name': 'LFR-t1=3;t2=1.5;mu=0.6;average_degree=5;'}),
+    #(SyntheticFormatter, False, {'path': DATA_PATH + 'synthetic/LFR/', 'dataset_name': 'LFR-t1=3;t2=1.5;mu=0.7;average_degree=5;'}),
+    #(SyntheticFormatter, False, {'path': DATA_PATH + 'synthetic/LFR/', 'dataset_name': 'LFR-t1=3;t2=1.5;mu=0.8;average_degree=5;'}),
+    #(SyntheticFormatter, False, {'path': DATA_PATH + 'synthetic/LFR/', 'dataset_name': 'LFR-t1=3;t2=1.5;mu=0.9;average_degree=5;'}),
+    #(SyntheticFormatter, False, {'path': DATA_PATH + 'synthetic/LFR/', 'dataset_name': 'LFR-t1=3;t2=1.5;mu=1;average_degree=5;'}),
+
+    (RealWorldAttrFormatter, True, {'path': DATA_PATH + 'real_world_data/', 'dataset_name': 'citeseer'}),
+    #(RealWorldAttrFormatter, True, {'path': DATA_PATH + 'real_world_data/', 'dataset_name': 'cora'}),
+    #(RealWorldAttrFormatter, True, {'path': DATA_PATH + 'real_world_data/', 'dataset_name': 'cora_ml'}),
+    #(RealWorldAttrFormatter, True, {'path': DATA_PATH + 'real_world_data/', 'dataset_name': 'dblp'}),
+    #(RealWorldAttrFormatter, True, {'path': DATA_PATH + 'real_world_data/', 'dataset_name': 'pubmed'}),
+
+    (RealWorldNonAttrFormatter, False, {'dataset_name': 'Malaria_var_DBLa_HVR_networks_HVR_networks_9'}),
+    #(RealWorldNonAttrFormatter, False, {'dataset_name': 'Email_network_Uni_R-V_Spain_Email_network_Uni_R-V_Spain'}),
+    #(RealWorldNonAttrFormatter, False, {'dataset_name': '595b15bd549f067e0263b525'}),
 ]
 
 
@@ -25,25 +55,26 @@ def __main__():
 
     for formatter_cls, attributed, args in datasets:
         data_path = PRE_COMPUTED_PATH + args['dataset_name'] + '/'
-        os.makedirs(data_path)
 
-        log.info(f'STARTING PRE-COMPUTING FOR {args["dataset_name"]}')
+        if not os.path.isdir(data_path):
+            log.info(f'STARTING PRE-COMPUTING FOR {args["dataset_name"]}')
+            data = formatter_cls(args, attributed).load_data()
 
-        formatter = formatter_cls(args, attributed)
+            log.info('WRITING DATA')
 
-        data = formatter.load_data()
+            os.makedirs(data_path)
 
-        log.info('WRITING DATA')
+            data['train_1'].dropna(inplace=True)
+            data['test_1'].dropna(inplace=True)
+            data['test_2'].dropna(inplace=True)
 
-        data['train_1'].dropna(inplace=True)
-        data['test_1'].dropna(inplace=True)
-        data['test_2'].dropna(inplace=True)
+            data['train_1'].to_csv(data_path + 'train_1.csv')
+            data['test_1'].to_csv(data_path + 'test_1.csv')
+            data['test_2'].to_csv(data_path + 'test_2.csv')
 
-        data['train_1'].to_csv(data_path + 'train_1.csv')
-        data['test_1'].to_csv(data_path + 'test_1.csv')
-        data['test_2'].to_csv(data_path + 'test_2.csv')
-
-        log.info('DONE')
+            log.info('DONE')
+        else:
+            log.warning(f'Directory {data_path} \nalready exists, skipping...')
 
 
 __main__()
